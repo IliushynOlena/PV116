0ececc4323930cb30fdb79350776271a18722c4b
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2022-11-30 17:50:06 +0200

    PictureBox

---
+++ 12_NotifyIcon/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _12_NotifyIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(431, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Count";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(164, 291);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(259, 92);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(648, 291);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(253, 102);
+            this.StopBtn.TabIndex = 2;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 679);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
---
+++ 12_NotifyIcon/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_NotifyIcon
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+
+            this.ShowInTaskbar = false;
+            notifyIcon1.Click += NotifyIcon1_Click;
+        }
+
+        private void NotifyIcon1_Click(object? sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+
+        }
+
+        private void Timer1_Tick(object? sender, EventArgs e)
+        {
+            label1.Text = (++count).ToString();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            timer1.Start();
+            StartBtn.Enabled = false;
+            StopBtn.Enabled = true;
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Таймер почав відлік!";
+            notifyIcon1.BalloonTipTitle = "Увага!";
+            notifyIcon1.ShowBalloonTip(2000);
+
+            //notifyIcon1.ShowBalloonTip(1000, "Увага!", "Таймер почав відлік!", ToolTipIcon.Info);
+
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            StartBtn.Enabled = true;
+            StopBtn.Enabled = false;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Warning;
+            notifyIcon1.BalloonTipText = "Таймер зупинився!";
+            notifyIcon1.BalloonTipTitle = "Увага!";
+            notifyIcon1.ShowBalloonTip(2000);
+           
+        }
+    }
+}
